d663807e2c3fb0d1a1343de7ccbba26b871bc4e1
Arglex <orisi1999@gmail.com>
2023-12-13 04:12:44 +0200

    Added abillity on 'X'

--- Solar_Ascension/Assets/MoreVision.cs
+++ Solar_Ascension/Assets/MoreVision.cs
@@ -15,6 +15,8 @@ public class MoreVision : MonoBehaviour
     [SerializeField] private float darknessCoolDown = 1.5f;
     [SerializeField] private float darknessAmount = 0.001f;
     [SerializeField] private float lightAmount = 0.06f;
+    [SerializeField] private float burstAbillityAmount = 0.15f;
+    [SerializeField] private float maxLight = 0.25f;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -27,10 +29,16 @@ public class MoreVision : MonoBehaviour
 
     public void BrighterTheScreen()
     {
-        vg.intensity.value -= lightAmount;
+        if (vg.intensity.value - lightAmount <= maxLight)
+        {
+            vg.intensity.value = maxLight;
+        }
+        else
+        {
+            vg.intensity.value -= lightAmount; 
+        }
     }
-
-    // Start is called before the first frame update
+    
     void Start()
     {
         darknnessCoolDownTimer = darknessCoolDown;
@@ -48,6 +56,23 @@ public class MoreVision : MonoBehaviour
         {
             vg.intensity.value += darknessAmount;
             darknessCoolDown = darknnessCoolDownTimer;
+        }
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            BurstAbillity();
+        }
+    }
+
+    private void BurstAbillity()
+    {
+        if (vg.intensity.value - lightAmount <= maxLight)
+        {
+            vg.intensity.value = maxLight;
+        }
+        else
+        {
+            vg.intensity.value -= burstAbillityAmount;
         }
     }
 }
